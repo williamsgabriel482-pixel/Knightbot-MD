985d6652f0f8cad5b486c9c124b67303dd3687ed
williamsgabriel482-pixel <williamsgabriel482@gmail.com>
2025-08-22 14:56:49 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Knight Bot', // Your name
+  botOwner: 'Sasuke', // Your name
   ownerNumber: '2348138168285', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
